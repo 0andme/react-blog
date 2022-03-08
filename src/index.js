49df7f97bceca56ff342374d86d4682f4d3e636c
0andme <yu00mi97@gmail.com>
2022-03-09 04:27:40 +0900

    #1 feat : reset.css 및 color.css 파일 연결

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "style/color.css";
+import "style/reset.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
