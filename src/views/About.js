c4953e05315fc7e899cc70b3bfd78891ac560287
0andme <yu00mi97@gmail.com>
2022-03-10 06:58:41 +0900

    #1 feat : about 페이지 헤더 변경

--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,11 @@
 import Container from "components/Container";
-import NavBar from "components/NavBar";
+import Topheader from "components/Topheader";
 import React from "react";
 
 function About() {
   return (
     <Container>
-      <NavBar isShowTitle={true} />
+      <Topheader />
     </Container>
   );
 }
