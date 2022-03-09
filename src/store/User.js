87b6477e10e36eec5f029e35a0c576bd6db91d5f
0andme <yu00mi97@gmail.com>
2022-03-09 21:08:40 +0900

    #1 feat : User data 전역 데이터로 지정
    사용자 명
    블로그 타이틀
    서브타이틀
    유저프로필파일명

--- src/store/User.js
+++ src/store/User.js
@@ -1,11 +1,22 @@
 import React, { createContext, useState } from "react";
 
 export const UserData = createContext({
-  name: "",
+  userName: "",
+  userGoal: "",
+  userImg: "",
+  blogTitle: "",
 });
 function User({ children }) {
-  const [name, setName] = useState("0andme");
-  return <UserData.Provider value={{ name }}>{children}</UserData.Provider>;
+  const [userName, setUserName] = useState("0andme");
+  const [userGoal, setUserGoal] = useState("LV.99 개발자 되기");
+  const [userImg, setUserImg] = useState("author-logo.jpeg");
+  const [blogTitle, setBlogTitle] = useState("0andme의 개발 블로그");
+
+  return (
+    <UserData.Provider value={{ userName, userGoal, userImg, blogTitle }}>
+      {children}
+    </UserData.Provider>
+  );
 }
 
 export default User;
