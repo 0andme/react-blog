73035474dbfcb73346c3a0d43e7b374d945d8cc5
0andme <yu00mi97@gmail.com>
2022-03-09 21:17:49 +0900

    #1 feat : about 및 tag 별 라우터 생성 및 컴포넌트 연결

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import About from "views/About";
 import Home from "views/Home";
+import TagPage from "views/TagPage";
 
 function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/:tag" element={<TagPage />} />
     </Routes>
   );
 }
