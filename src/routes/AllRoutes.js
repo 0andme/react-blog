8e37a0a73fba8245c87a94d317d36024ed825bf2
0andme <yu00mi97@gmail.com>
2022-03-10 08:11:14 +0900

    #1 feat : 포스트 상세 페이지 라우터 및 컴포넌트 생성

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "views/About";
 import Home from "views/Home";
+import PostDetail from "views/PostDetail";
 import TagPage from "views/TagPage";
 
 function AllRoutes() {
@@ -10,6 +11,7 @@ function AllRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/:tag" element={<TagPage />} />
+      <Route path="/post/:postdate" element={<PostDetail />} />
     </Routes>
   );
 }
---
+++ src/views/PostDetail.js
@@ -0,0 +1,13 @@
+import Container from "components/Container";
+import Topheader from "components/Topheader";
+import React from "react";
+
+function PostDetail() {
+  return (
+    <Container>
+      <Topheader></Topheader>
+    </Container>
+  );
+}
+
+export default PostDetail;
