53e7e4f37e1f76aaa0e1e698a34238e4efae8c04
0andme <yu00mi97@gmail.com>
2022-03-09 21:13:35 +0900

    #1 feat : 포스트 카드 컴포넌트 추가

---
+++ src/components/PostCard.js
@@ -0,0 +1,104 @@
+import React, { useContext } from "react";
+import { UserData } from "store/User";
+import styled from "styled-components";
+import { v1 as uuid } from "uuid";
+function PostCard({ post }) {
+  const { userImg, userName } = useContext(UserData);
+  return (
+    <PostCardStyle>
+      <img
+        className="cover"
+        src={require(`assets/postCovers/${post.cover}`)}
+        alt={post.title}
+      />
+      <div className="main">
+        <div className="tags">
+          {post.tags.map((tag) => {
+            return <span key={uuid()}>{tag}</span>;
+          })}
+        </div>
+        <div className="title">{post.title}</div>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+        <div className="author">
+          <div className="imgBox">
+            <img src={require(`assets/${userImg}`)} alt="프로필" />
+          </div>
+          <span>{userName}</span>
+        </div>
+      </div>
+    </PostCardStyle>
+  );
+}
+const PostCardStyle = styled.div`
+  width: calc((100% - 20px) / 2);
+  max-width: calc((1040px - 20px) / 2);
+
+  box-sizing: border-box;
+  overflow: hidden;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  background-color: var(--white);
+  &:first-child {
+    width: 100%;
+    max-width: calc(1040px);
+    display: flex;
+    .cover {
+      width: 50%;
+    }
+    .main {
+      display: flex;
+      height: 100%;
+
+      flex-direction: column;
+      width: 50%;
+      .content {
+        flex: 1;
+      }
+    }
+  }
+  .cover {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  .tags {
+    display: flex;
+    align-items: center;
+    font-size: 0.5rem;
+    opacity: 0.5;
+    margin-bottom: 10px;
+  }
+  .title {
+    font-size: 1.5em;
+    margin-bottom: 10px;
+  }
+  .content {
+    width: 100%;
+    overflow: hidden;
+    height: 100%;
+    text-overflow: ellipsis;
+    opacity: 0.6;
+    font-size: 0.8em;
+    margin-bottom: 15px;
+    white-space: nowrap;
+  }
+  .author {
+    font-size: 0.8em;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    .imgBox {
+      width: 30px;
+      height: 30px;
+      border-radius: 30px;
+      overflow: hidden;
+      img {
+        width: 100%;
+      }
+    }
+  }
+`;
+export default PostCard;
