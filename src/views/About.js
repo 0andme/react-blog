14dd786fb7068449ea1b538ef5c37361cf7731ea
0andme <yu00mi97@gmail.com>
2022-03-09 21:06:44 +0900

    #1 feat :  About 페이지 추가
    네이게이션 바 추가

---
+++ src/views/About.js
@@ -0,0 +1,13 @@
+import Container from "components/Container";
+import NavBar from "components/NavBar";
+import React from "react";
+
+function About() {
+  return (
+    <Container>
+      <NavBar isShowTitle={true} />
+    </Container>
+  );
+}
+
+export default About;
